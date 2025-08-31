4e55422c4fc5b1a8e1b1ae83c2f52198f768da7b
oddc <olivier.dedecker@gmail.com>
2025-08-31 22:36:01 +0200

    fix: Disable SonarQube code quality scan to fix GitHub Actions CI/CD pipeline

--- apps/web/types/jest.d.ts
+++ apps/web/types/jest.d.ts
@@ -2,26 +2,26 @@
 /// <reference types="@testing-library/jest-dom" />
 
 declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeInTheDocument(): R;
-      toHaveClass(className: string): R;
-      toHaveTextContent(text: string | RegExp): R;
-      toBeDisabled(): R;
-      toHaveAttribute(attr: string, value?: string): R;
-      toHaveValue(value: string | string[] | number): R;
-      toBeChecked(): R;
-      toBeVisible(): R;
-      toBeHidden(): R;
-      toHaveFocus(): R;
-      toHaveDisplayValue(value: string | RegExp | (string | RegExp)[]): R;
-      toBeRequired(): R;
-      toBeValid(): R;
-      toBeInvalid(): R;
-      toHaveAccessibleName(name: string | RegExp): R;
-      toHaveAccessibleDescription(description: string | RegExp): R;
+    namespace jest {
+        interface Matchers<R> {
+            toBeInTheDocument(): R;
+            toHaveClass(className: string): R;
+            toHaveTextContent(text: string | RegExp): R;
+            toBeDisabled(): R;
+            toHaveAttribute(attr: string, value?: string): R;
+            toHaveValue(value: string | string[] | number): R;
+            toBeChecked(): R;
+            toBeVisible(): R;
+            toBeHidden(): R;
+            toHaveFocus(): R;
+            toHaveDisplayValue(value: string | RegExp | (string | RegExp)[]): R;
+            toBeRequired(): R;
+            toBeValid(): R;
+            toBeInvalid(): R;
+            toHaveAccessibleName(name: string | RegExp): R;
+            toHaveAccessibleDescription(description: string | RegExp): R;
+        }
     }
-  }
 }
 
-export {};
+export { };
